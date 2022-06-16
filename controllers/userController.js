0928cfab478df881d80ae4d7758972e0e5715cee
Dishant <dishant.rathee@gmail.com>
2022-06-16 17:41:38 +0530

    route added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const mysql=require('mysql2/promise');
 const config=require('../config');
 const bcrypt=require('bcrypt');
 
+var logged=false;
+var loggedUser='';
+
 async function query(sql){
     const connection = await mysql.createConnection(config.db);
     const [results, ] = await connection.execute(sql);
@@ -30,14 +33,44 @@ const login=async (req,res)=>{
         );
     
     console.log(row);
+    if(row.length!=0){
 
-    const salt=row[0]["salt"];
-    const pass2=await  bcrypt.hash(pass,salt);
-    if(pass2==row[0]["password"])
-        console.log('Authentication Successful! Logged In');
-    else
-        console.log('Incorrect Password!');
-    res.send(req.body);
+        const salt=row[0]["salt"];
+        const pass2=await  bcrypt.hash(pass,salt);
+        if(pass2==row[0]["password"]){
+            console.log('Authentication Successful! Logged In');
+            logged=true;
+            loggedUser=user;
+        }
+        else
+            console.log('Incorrect Password!');
+        }
+        else
+            console.log('user does not exist!');
+        res.send(req.body);
+    };
+
+const startup_details=async (req,res)=>{
+    const {user,details,cf1,cf2,cto,cfo,cmo,contact,cf1name,cf1linkedin,cf1link,cf2name,cf2linkedin,cf2link,ctoname,ctolinkedin,ctolink,cfoname,cfolinkedin,cfolink,cmoname,cmolinkedin,cmolink}=req.body;
+    if(logged&&user==loggedUser){
+        await query(
+            `INSERT into startup(username,details,cf1,cf2,cto,cfo,cmo,contact) VALUES("${loggedUser}","${details}","${cf1}","${cf2}","${cto}","${cfo}","${cmo}","${contact}")`
+        );
+
+        const temp=[[cf1,cf1name,cf1linkedin,cf1link],[cf2,cf2name,cf2linkedin,cf2link],[cto,ctoname,ctolinkedin,ctolink],[cfo,cfoname,cfolinkedin,cfolink],[cmo,cmoname,cmolinkedin,cmolink]]
+        temp.forEach(async element => {
+            console.log(element);
+            await query(
+                `INSERT into members(email,name,linkedin,other) VALUES("${element[0]}","${element[1]}","${element[2]}","${element[3]}")`
+            );
+        });
+        console.log("Insertion successfull!");
+        res.send({"user":user});
+    }
+    else{
+        console.log("First Login!");
+        res.send({"msg":"Login"});
+    }
 };
 
-module.exports={signup,login};
+module.exports={signup,login,startup_details};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express=require('express');
-const {signup,login}=require('../controllers/userController');
+const {signup,login,startup_details}=require('../controllers/userController');
 const Router=express.Router();
 
 Router.route('/signup').post(signup);
 Router.route('/login').post(login);
+Router.route('/register').post(startup_details);
 
 module.exports=Router;
 
